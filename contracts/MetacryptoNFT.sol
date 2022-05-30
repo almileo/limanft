36e2b1ce72a422ab5643e48f55de9a1b26c6dc54
almileo <leonardo.almiron@tutenlabs.com>
2022-05-30 12:28:26 -0300

    Initial commit - dependencies instalations - nft contract

---
+++ contracts/MetacryptoNFT.sol
@@ -0,0 +1,61 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+import "hardhat/console.sol";
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract MetaCryptoNFT is ERC721, ERC721Enumerable, ERC721URIStorage {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+    uint256 MAX_SUPPLY = 1000;
+    uint256 MAX_MINTING = 6;
+    mapping (address => uint256) addressToNumberOfNft;
+
+    constructor() ERC721("MetaCrypt0NFT", "MC0") {}
+
+    function safeMint(address to, string memory uri) public {
+        uint256 tokenId = _tokenIdCounter.current();
+        require(addressToNumberOfNft[msg.sender] < MAX_MINTING, "Your wallet already has many NFT");
+        require(tokenId <= MAX_SUPPLY, "There are no more NFT to mint, wait for the next batch.");
+        _tokenIdCounter.increment();
+        addressToNumberOfNft[msg.sender] += 1;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
