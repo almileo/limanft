922ce8bb69b08b66bc3598bce4da6f3ecd104f21
almileo <leonardo.almiron@tutenlabs.com>
2022-05-30 15:33:08 -0300

    verifying the contract.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,11 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 
 const GOERLI_URL = process.env.GOERLI_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -15,5 +17,10 @@ module.exports = {
       url: GOERLI_URL,
       accounts: [PRIVATE_KEY]
     }
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: ETHERSCAN_API_KEY
   }
 };
